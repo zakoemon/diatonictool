d31143ca6fa9522b1abf1517d261406f062eae54
zakoemon <zoemon90@gmail.com>
2023-01-29 13:51:24 +0900

    update: interface{}->any

--- internal/jsontool/jsontool.go
+++ internal/jsontool/jsontool.go
@@ -5,11 +5,11 @@ import (
 	"os"
 )
 
-type Convert[T interface{}] interface {
+type Convert[T any] interface {
 	Convert(data []byte) (T, error)
 }
 
-func Read[T interface{}](path string, converter Convert[T]) (T, error) {
+func Read[T any](path string, converter Convert[T]) (T, error) {
 	bytes, err := os.ReadFile(path)
 	if err != nil {
 		log.Fatal(err)
